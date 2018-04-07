498ca0c4076ce1f786a665364bf5a43ecbd33999
Ritwick Biswas <ritwick.biswas95@gmail.com>
2018-04-07 20:14:23 +0530

    Initial setup

---
+++ nooblms/gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+//dependenceis
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var changed = require('gulp-changed');
+
+var SCSS_SRC = './src/Assets/scss/**/*.scss';
+var SCSS_DEST = './src/Assets/css';
+
+gulp.task('compile_scss',function(){
+  gulp.src(SCSS_SRC);
+  .pipe(scss().on('error',sass.logError))
+  .pipe(minifyCSS())
+  .pipe(rename({suffix:'.min'}))
+  .pipe(changed(SCSS_DEST))
+  .pipe(gulp.dest(SCSS_DEST));
+});
